a566521b9d5eb71c96f89b7a340932193e332c49
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-08-30 19:29:13 +0330

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
   console.log("workout tracker app started");
   console.log("this is something addd to test");
   console.log("comment for test 2");
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold underline">workout tracker</h1>
