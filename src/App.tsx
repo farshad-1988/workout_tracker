e53926329ff3cf35841f87f5a5116dd7a91c2962
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-08-30 18:07:36 +0330

    initial deploy

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  console.log("workout tracker app started");
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold underline">workout tracker</h1>
+      <div className="h-96 w-96 text-center">
+        welcome to workout tracker App
+      </div>
+    </div>
+  );
+}
+
+export default App;
